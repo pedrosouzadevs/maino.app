fbec3c30d17668357a4c3111a6003f2fc6ceed32
Pedro Souza <phmartinsdesouza@gmail.com>
2025-01-06 16:12:47 -0300

    just preparing to send to heroku

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,5 @@
 class SearchController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
   def index
     @query = Post.ransack(params[:q])
     @posts = @query.result.page(params[:page])
